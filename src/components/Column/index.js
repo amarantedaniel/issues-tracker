110a765f41a95ff1fa09f5f79dc286e6139fc193
Daniel Amarante <daniel.amarante2@gmail.com>
2019-05-06 23:59:10 -0300

    Move mutation logic from column to issue

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import { Mutation } from 'react-apollo'
 import Issue from 'components/Issue'
 import map from 'lodash/map'
-import { closeIssue, reopenIssue } from 'api/queries'
 import './column.scss'
 
-const Column = ({ issues = [], type }) => {
-  const toggle = type === 'OPEN' ? closeIssue : reopenIssue
-  return (
-    <div className="flex flex-column column">
-      <h2 className="column__title">{type}</h2>
-      <Mutation mutation={toggle}>
-        {toggle => (
-          <div className="column__body">
-            {map(issues, issue => (
-              <Issue
-                key={issue.id}
-                issue={issue}
-                onClick={() => {
-                  toggle({ variables: { issueId: issue.id } })
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </Mutation>
+const Column = ({ issues = [], type }) => (
+  <div className="flex flex-column column">
+    <h2 className="column__title">{type}</h2>
+    <div className="column__body">
+      {map(issues, issue => (
+        <Issue key={issue.id} issue={issue} />
+      ))}
     </div>
-  )
-}
+  </div>
+)
 
 export default Column
--- src/components/Issue/index.js
+++ src/components/Issue/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import cn from 'classnames'
+import { closeIssue, reopenIssue } from 'api/queries'
 import './issue.scss'
 
-const Issue = ({ issue, onClick }) => (
-  <div className="flex flex-column items-center issue">
-    <span>{issue.title}</span>
-    <span>{issue.state}</span>
-    <button onClick={onClick}>toggle</button>
-  </div>
-)
+const Issue = ({ issue }) => {
+  const toggle = issue.state === 'OPEN' ? closeIssue : reopenIssue
+  return (
+    <Mutation mutation={toggle}>
+      {(toggle, { loading }) => (
+        <div
+          className={cn(
+            'flex flex-column items-center issue',
+            loading && 'issue--loading',
+          )}
+        >
+          <span>{issue.title}</span>
+          <span>{issue.state}</span>
+          <button
+            onClick={() => {
+              toggle({ variables: { issueId: issue.id } })
+            }}
+          >
+            toggle
+          </button>
+        </div>
+      )}
+    </Mutation>
+  )
+}
 
 export default Issue
