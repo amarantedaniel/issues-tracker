f31687d987ae1ce49417a6cf4b6d39d1cbd86193
Daniel Amarante <daniel.amarante2@gmail.com>
2019-05-11 11:05:58 -0300

    Add css framework to make project prettier

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => (
+  <button onClick={() => console.log('add issue')}>Add Issue</button>
+)
+
+export default Header
---
+++ src/scenes/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Board from 'components/Board'
+import Header from 'components/Header'
+
+const Home = () => (
+  <div>
+    <Header />
+    <Board />
+  </div>
+)
+
+export default Home
