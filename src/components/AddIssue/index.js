0ef35b609ac1bf2b59dcbe6f31c5ede889257382
Daniel Amarante <daniel.amarante2@gmail.com>
2019-05-11 15:06:50 -0300

    Make issues be created by form

--- src/components/AddIssue/index.js
+++ src/components/AddIssue/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
 import Modal from 'components/Modal'
+import { createIssue } from 'api/queries'
+import { addIssueToCache } from 'api/cache'
+import { parseRepository } from 'api/parser'
 
-const AddIssue = ({ open, onClose }) => (
+const AddIssueModal = ({ open, onClose, data }) => (
   <Modal open={open} onClose={onClose}>
-    <div>das</div>
+    <AddIssue data={data} />
   </Modal>
 )
 
-export default AddIssue
+const AddIssue = ({ data }) => (
+  <Mutation mutation={createIssue} update={addIssueToCache}>
+    {createIssue => {
+      const handleSubmit = event => {
+        event.preventDefault()
+        const { id } = parseRepository(data)
+        createIssue({
+          variables: { repositoryId: id, title: 'I have created a new thing' },
+        })
+      }
+      return (
+        <form className="flex flex-column" onSubmit={handleSubmit}>
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      )
+    }}
+  </Mutation>
+)
+
+export default AddIssueModal
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import { Mutation } from 'react-apollo'
-import { parseRepository } from 'api/parser'
-import { createIssue } from 'api/queries'
-import { addIssueToCache } from 'api/cache'
+import React, { useState } from 'react'
 import AddIssue from 'components/AddIssue'
 
-const Header = ({ data }) => (
-  <Mutation mutation={createIssue} update={addIssueToCache}>
-    {createIssue => {
-      const handleClick = () => {
-        const { id } = parseRepository(data)
-        createIssue({
-          variables: { repositoryId: id, title: 'I have created a new thing' },
-        })
-      }
-      return (
-        <div>
-          <button onClick={handleClick}>Add Issue</button>
-          <AddIssue />
-        </div>
-      )
-    }}
-  </Mutation>
-)
+const Header = ({ data }) => {
+  const [addIssueOpened, setAddIssueOpened] = useState(false)
+
+  const openAddIssue = () => {
+    setAddIssueOpened(true)
+  }
+  const closeAddIssue = () => {
+    setAddIssueOpened(false)
+  }
+  return (
+    <div>
+      <button onClick={openAddIssue}>Add Issue</button>
+      <AddIssue onClose={closeAddIssue} open={addIssueOpened} data={data} />
+    </div>
+  )
+}
 
 export default Header
